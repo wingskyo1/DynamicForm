c192359428e2322f83c41dcc0aafa4beb9de7758
Wing <wingskyo1@gmail.com>
2019-07-26 11:25:29 +0800

    Add user interface to create form

---
+++ DynamicForm/ClientApp/components/createComponent/createInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex four">
+    <div class="three-fourth">
+      <input type="text" placeholder="輸入內容" v-model="data.name">
+    </div>
+    <div>
+      <button class='error' @click="delRecord(data.id)">Del</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      data: {
+        type: Object,
+        default: () => {
+          return {
+            id: '',
+            name: '',
+          };
+        },
+      },
+    },
+    methods: {
+      delRecord: function (id) {
+        this.$emit('delRecord', id);
+      },
+    },
+    created(){
+      console.log(this.data)
+    }
+  };
+</script>
