6ecd89fdf21cdacdcdbbdfc73558ecb9d3cd4906
wing <wingskyo1@gmail.com>
2019-07-29 18:04:06 +0800

    update

---
+++ data/BaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using data.Entities;
+
+namespace data
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext(DbContextOptions<BaseContext> options) : base(options)
+        {
+            
+        }
+        
+        DbSet<Questionnaire> Questionnaire { get; set; }  
+
+    }
+}
---
+++ data/Entities/Questionnaire.cs
@@ -0,0 +1,10 @@
+namespace data.Entities
+{
+    public class Questionnaire
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }  
+        public string JsonData { get; set; }  
+
+    }
+}
