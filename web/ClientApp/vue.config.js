38df3d05d5d0eeb71abfc8f8ba87e16e14c39156
wing <wingskyo1@gmail.com>
2019-07-30 11:34:42 +0800

    Remove RiderTest Setting

--- web/ClientApp/vue.config.js
+++ web/ClientApp/vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     outputDir: 'dist',
-    publicPath: '/',
+    publicPath: '/'
     //required to fix sockjs-node SSL error
     //https://stackoverflow.com/questions/50877800/how-can-i-fix-these-sockjs-ssl-errors
-    devServer: {
-        public: 'https://localhost:5001'
-    }
+    // devServer: {
+    //     public: 'https://localhost:5001'
+    // }
 };
